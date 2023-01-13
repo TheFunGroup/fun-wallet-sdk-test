d46ebda46f5fe2d74a5fc0b96e88a609aa18c0ed
Your Name <you@example.com>
2023-01-13 13:44:09 -0800

    updated via notes flow and schema

--- src/App.js
+++ src/App.js
@@ -1,29 +1,43 @@
 import logo from './logo.svg';
+
 import './App.css';
 import { ethers } from 'ethers'
 import { FunWallet, AAVEWithdrawal, AccessControlSchema } from "@fun-wallet/sdk"
+import { useEffect, useState } from 'react';
 const rpc = "https://avalanche-fuji.infura.io/v3/4a1a0a67f6874be6bb6947a62792dab7"
-// const aTokenAddress = "0xC42f40B7E22bcca66B3EE22F3ACb86d24C997CC2" // Avalanche Fuji AAVE Dai
 
 
 function App() {
+  const [aTokenAddress, setATokenAddress] = useState("0xC42f40B7E22bcca66B3EE22F3ACb86d24C997CC2") // Avalanche Fuji AAVE Dai
+
+  const [pkey, setPkey] = useState("") // avax-fuji
+  const [eoa, setEoa] = useState({})
+  const [walletType, setWalletType] = useState(false)
+
+  useEffect(() => {
+    if (pkey) {
+
+    }
+  }, [pkey])
   const handleClick = async () => {
 
     // Create an EOA instance with ethers
 
     // With metamask
+    let eoa;
 
-    const provider = new ethers.providers.Web3Provider(window.ethereum)
-    await provider.send('eth_requestAccounts', []); // <- this promps user to connect metamask
-    const eoa = provider.getSigner();
+    if (walletType) {
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      await provider.send('eth_requestAccounts', []); // <- this promps user to connect metamask
+      eoa = provider.getSigner();
+    }
+    else {
 
-    // With privateKey
+      // With privateKey
+      const provider = new ethers.providers.JsonRpcProvider(rpc)
+      eoa = new ethers.Wallet(pkey, provider)
+    }
 
-    // const privKey = "c5ff68eee74d447b88d5d0dd1d438b37f30a4c0b1e41e9c485c6e2ea282d1489"
-    // const provider = new ethers.providers.JsonRpcProvider(rpc)
-    // const eoa = new ethers.Wallet(privKey, provider)
-
-    const aTokenAddress = "0xC42f40B7E22bcca66B3EE22F3ACb86d24C997CC2" // Avalanche Fuji AAVE Dai
 
     const schema = new AccessControlSchema()
 
@@ -33,8 +47,9 @@ function App() {
 
     // Create a new FunWallet instance, 
     const prefundAmt = 0 // eth
-    const chain = '43113'
+
     // Initialize the FunWallet instance, initially funded with 0.3 AVAX to cover gas fees
+    const chain = "43113"
     const wallet = await schema.createFunWallet(eoa, prefundAmt, chain)
 
     const createWalletReceipt = await wallet.initializeWallet()
@@ -60,12 +75,17 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-
-        <button onClick={handleClick}>
-          test
+        <div>
+          <label>Click to switch</label>
+          <button className="data-button" onClick={() => { setWalletType(!walletType) }}>{walletType ? "Using Metamask" : "Using Private Key"}</button>
+        </div>
+        {!walletType &&
+          <input placeholder='Enter Private Key. KEYS ARE NOT STORED OR SAVED' value={pkey} onChange={({ target }) => { setPkey(target.value) }} className="data-input" />
+        }
+        <label>AToken Address: Default AaveDai</label>
+        <input value={aTokenAddress} onChange={({ target }) => { setATokenAddress(target.value) }} className="data-input" />
+        <button onClick={handleClick} className="data-button">
+          Run Test
         </button>
       </header>
     </div>
