e5f44c5e6d7ad89748b7d5232e1edaae55bd857b
aaronchewbani <aaronchewbani@gmail.com>
2023-02-19 14:09:08 -0800

    init

--- src/App.js
+++ src/App.js
@@ -2,56 +2,79 @@ import './App.css';
 import logo from './logo.svg'
 import { ethers } from 'ethers'
 import { useState } from 'react'
-import { FunWallet, AAVEWithdrawal, AccessControlSchema } from "@fun-wallet-dev/sdk"
+import { FunWallet, FunWalletConfig, Modules } from "@fun-wallet/sdk";
+// import { FunWallet, FunWalletConfig, Modules } from "./fun-wallet-dev/sdk/index";
 
+const getBalance = async (wallet) => {
+  const balance = await wallet.provider.getBalance(wallet.address);
+  return ethers.utils.formatUnits(balance, 18)
+}
 
-const rpcURL = "https://avalanche-fuji.infura.io/v3/4a1a0a67f6874be6bb6947a62792dab7"
-const chainID = '43113' // Fuji testnet ChainID
-const prefundAmt = 0.3
-
+const transferAmt = async (signer, to, value) => {
+  const tx = await signer.sendTransaction({ to, value: ethers.utils.parseEther(value.toString()) })
+  await tx.wait()
+  console.log(`${await signer.getAddress()} == ${value} => ${to}`)
+}
 function App() {
+  const HARDHAT_FORK_CHAIN_ID = 31337
+  const RPC_URL = "http://127.0.0.1:8545"
+  const PRIV_KEY = "0x66f37ee92a08eebb5da72886f3c1280d5d1bd5eb8039f52fdb8062df7e364206"
+  const PKEY = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
+  const DAI_ADDR = "0x6B175474E89094C44Da98b954EedeAC495271d0F"
+  const API_KEY = "hnHevQR0y394nBprGrvNx4HgoZHUwMet5mXTOBhf"
+  const USDC_ADDR = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
+  const AVAX_RPC_URL = "https://avalanche-fuji.infura.io/v3/4a1a0a67f6874be6bb6947a62792dab7"
+  const AVAX_CHAIN_ID = "43113"
+  const USDC_AVAX_ADDR = "0x5425890298aed601595a70AB815c96711a31Bc65"
+  const AVAX_RECEIVE_PKEY = '3ef076e7d3d2e1f65ded46b02372d7c5300aec49e780b3bb4418820bf068e732'
+  const AMOUNT = 60
+  const PREFUND_AMT = 0
+  const chainID = "43113"
+  const TOKEN_ADDRESS = DAI_ADDR
+  const WITHDRAW_AMOUNT = ethers.constants.MaxInt256
+
+  const walletTransferERC = async (wallet, to, amount, tokenAddr) => {
+    const transfer = new Modules.TokenTransfer()
+
+    await wallet.addModule(transfer)
+    const transferActionTx = await transfer.createTransferTx(to, amount, { address: tokenAddr })
+    const receipt = await wallet.deployTx(transferActionTx)
+    console.log(receipt)
+
+}
+
+  const handleClick = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    await provider.send('eth_requestAccounts', []); // <- this promps user to connect metamask
+    const eoa = provider.getSigner();
+
+
+    const config = new FunWalletConfig(eoa, chainID, PREFUND_AMT);
+    console.log("config111111", config)
+    console.log(await config.getChainInfo())
+    const wallet = new FunWallet(config, API_KEY);
+    await wallet.init()
+    console.log(wallet.address)
+    await walletTransferERC(wallet, "0xDc054C4C5052F0F0c28AA8042BB333842160AEA2", "100", USDC_AVAX_ADDR) //1000000 = 1usdc
+
+    // const eoaAaveWithdrawalModule = new Modules.EoaAaveWithdrawal();
+    // wallet.addModule(eoaAaveWithdrawalModule);
+
+    // const deployWalletReceipt = await wallet.deploy();
+
+    // const moduleRequiredPreTxs = eoaAaveWithdrawalModule.getPreExecTxs(deployWalletReceipt.address);
+    // const reqPreTxReceipt = await FunWallet.deployTxs(moduleRequiredPreTxs);
+    // console.log(reqPreTxReceipt)
+
+    // const aaveWithdrawTx = await eoaAaveWithdrawalModule.createWithdrawTx(TOKEN_ADDRESS, wallet.eoa.address, WITHDRAW_AMOUNT)
+    // const aaveWithdrawalReceipt = await FunWallet.deployTx(aaveWithdrawTx);
+    // console.log(aaveWithdrawalReceipt)
+  }
+
+  //outdated stuff
   const [aTokenAddress, setATokenAddress] = useState("0xC42f40B7E22bcca66B3EE22F3ACb86d24C997CC2") // Avalanche Fuji AAVE Dai
   const [pkey, setPkey] = useState("") // avax-fuji
   const [walletType, setWalletType] = useState(false)
-
-  const handleClick = async () => {
-
-    // Create an EOA instance
-    let eoa;
-    if (walletType) { // with metamask
-      const provider = new ethers.providers.Web3Provider(window.ethereum)
-      await provider.send('eth_requestAccounts', []); // <- this promps user to connect metamask
-      eoa = provider.getSigner();
-    }
-    else { // with privateKey
-      const provider = new ethers.providers.JsonRpcProvider(rpcURL)
-      eoa = new ethers.Wallet(pkey, provider)
-    }
-
-    // Create an access control schema with one action: withdraw a user's funds from Aave
-    const schema = new AccessControlSchema()
-    const withdrawEntirePosition = schema.addAction(AAVEWithdrawal(aTokenAddress))
-
-    // Create a FunWallet with the above access control schema, prefunded with prefundAmt AVAX
-    const wallet = new FunWallet(eoa, schema, prefundAmt, chainID)
-
-    // Deploy the FunWallet
-    const walletDeployReceipt = await wallet.deploy()
-    console.log("Creation Succesful:\n", walletDeployReceipt)
-
-    // Create a tx that exits an EOA's Aave poisition to be called at a later point
-    const aaveActionTx = await wallet.createActionTx(withdrawEntirePosition)
-
-    // Create & deploy a tx that gives the FunWallet authorization to close the EOA's Aave position
-    const tokenApprovalReceipt = await wallet.deployTokenApproval(aTokenAddress)
-    console.log("Approval Succesful:\n", tokenApprovalReceipt)
-
-    // After some time, deploy the Aave withdrawal action
-    const aaveWithdrawalReceipt = await FunWallet.deployActionTx(aaveActionTx)
-    console.log("Execution Succesful:\n", aaveWithdrawalReceipt)
-
-  }
-
   return (
     <div className="App">
       <header className="App-header">
